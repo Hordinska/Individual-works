d627b5e03f5641a8891007d888e02c442b551255
Hordinska <62242332+Hordinska@users.noreply.github.com>
2021-03-04 12:33:53 +0200

    Add files via upload

---
+++ IW№1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+//int main(){
+//    int a;
+//    cout << "n="; cin >> a;
+//    for (int i = a; i > 0; i--) {
+//        for (int j = i, k = 1; j > 0, k<=i; j--, k++) {
+//            cout << k <<" ";
+//        }
+//        cout << endl;
+//    }
+//
+//}
+/*
+n=3
+1 2 3
+1 2
+1 */
